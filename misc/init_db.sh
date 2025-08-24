de1a75e77aae38650ffca92b4a51ac20ad43a8c3
Josh Long <54473+joshlong@users.noreply.github.com>
2025-08-24 15:21:03 -0700

    yo

--- misc/init_db.sh
+++ misc/init_db.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+docker compose up -d 
+sleep 5
 cat `dirname $0`/legacy-schema.sql | PGPASSWORD=secret psql -U myuser -h localhost mydatabase
 
 # if you want to use the SQL then remember to run:
